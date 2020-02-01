de87dda3dbfbc553902d6254f2e3b42018a3aed7
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 07:12:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,20 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [4,5,6],
+  [7,8,9],
+  [0,4,7],
+  [1,5,8],
+  [2,6,9],
+  [0,5,9],
+  [2,5,7]
+]
+
+def won?(board)
+  for each combi in WIN_COMBINATIONS{
+    if board[combi[0]] == "X" && board[combi[1]] == "X" && board[combi[2]] == "X"  
+      puts combi
+  }
+end
