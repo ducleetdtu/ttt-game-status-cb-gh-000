01f7e2aa86ac0bcc502eaedaa84644cce036f432
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 07:39:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,14 @@ WIN_COMBINATIONS = [
 def won?(board)
   result = false
   WIN_COMBINATIONS.each{ |win_combination|
-    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+ 
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+    if position_1 == "X" && position_1 == "X" && position_1 == "X"
       result = win_combination
     else
       false
