ef972ed92d97c223c174bc11ca6b3f4fdbb6cf4f
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 07:37:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  result = nil
+  result = false
   WIN_COMBINATIONS.each{ |win_combination|
     if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
       result = win_combination
