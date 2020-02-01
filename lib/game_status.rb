c02969ded539a59ce5162ed879b0f1c61501274c
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 07:14:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  for each combi in WIN_COMBINATIONS
+  for each win_combination in WIN_COMBINATIONS
     if board[combi[0]] == "X" && board[combi[1]] == "X" && board[combi[2]] == "X"
       puts combi
     else
